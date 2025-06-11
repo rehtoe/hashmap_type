9e5c620c1ad77b6d90a8af01e212567727dadf9b
Ernesto <ejp1077@gmail.com>
2025-06-11 00:54:19 -0700

    updated main to test for hash of numbers 1-100

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
-#include <vector>
-
 #include "source/hash.h"
+#include "source/pyScript.h"
 
 int main(){
-    DataNode t0(0), t1(1), t2(1), t3(2), t4(0);
     DataHash hash;
 
+    /*
+    DataNode t0(0), t1(1), t2(1), t3(2), t4(0);
     hash.addNode(&t0);
     hash.addNode(&t1);
     hash.addNode(&t2);
     hash.addNode(&t3);
     hash.addNode(&t4);
+    */
+    
+    for(int i = 1; i <= 100; i++){
+        DataNode *phNode = new DataNode(i);
+        hash.addNode(phNode);
+    }
+    
 
     hash.printMap();
-
     return 0;
 }
