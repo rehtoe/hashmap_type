337385d9f3f122b3f3c61896327cd5dd92039861
Ernesto <ejp1077@gmail.com>
2025-06-11 00:09:40 -0700

    .

--- main.cpp
+++ main.cpp
@@ -2,16 +2,10 @@
 
 #include "source/hash.h"
 
-enum Attribute{
-    NONE=0,
-    HOLY=1,
-    DARK=2,
-};
-
 int main(){
     DataNode t0(0), t1(1), t2(1), t3(2), t4(0);
     DataHash hash;
-    
+
     hash.addNode(&t0);
     hash.addNode(&t1);
     hash.addNode(&t2);
