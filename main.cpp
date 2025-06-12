cfa282d6879653f7bfc08a213505bfcf5c131a7d
Ernesto <84341010+rehtoe@users.noreply.github.com>
2025-06-11 21:55:42 -0700

    Update main.cpp
    fixed main cpp to not have test / example with 1-100, made custom one instead

--- main.cpp
+++ main.cpp
@@ -4,20 +4,13 @@
 int main(){
     DataHash hash;
 
-    /*
     DataNode t0(0), t1(1), t2(1), t3(2), t4(0);
+    
     hash.addNode(&t0);
     hash.addNode(&t1);
     hash.addNode(&t2);
     hash.addNode(&t3);
     hash.addNode(&t4);
-    */
-    
-    for(int i = 1; i <= 100; i++){
-        DataNode *phNode = new DataNode(i);
-        hash.addNode(phNode);
-    }
-    
 
     hash.printMap();
     return 0;
